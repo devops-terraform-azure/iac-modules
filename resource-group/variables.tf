10f651ae1243a1b3a3f98a738cefc636402fe78f
developer <ukohae39@gmail>
2024-11-03 22:16:40 -0500

    Resource Group

---
+++ resource-group/variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group_name" {
+  description = "The Name which should be used for this Resource Group"
+  type        = string
+  default     = "rg-devsecops"
+}
+
+variable "location" {
+  description = "The Azure Region where the Resource Group should exist"
+  type        = string
+  default     = "eastus"
+}
+
+variable "tags" {
+  description = "A mapping of tags which should be assigned to the Resource Group"
+  type        = map()
+  default     = {}
+}
