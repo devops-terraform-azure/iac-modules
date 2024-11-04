566cd7ad65b91069b242c1c3b810e9888aca5118
developer <ukohae39@gmail>
2024-11-03 22:42:30 -0500

    Resource Group Location

--- resource-group/output.tf
+++ resource-group/output.tf
@@ -7,3 +7,8 @@ output "resource_group_id" {
   description = "The resource group id"
   value       = azurerm_resource_group.rg.id
 }
+
+output "resource_group_location" {
+  description = "The Resource Group Location."
+  value       = azurerm_resource_group.rg.location
+}
